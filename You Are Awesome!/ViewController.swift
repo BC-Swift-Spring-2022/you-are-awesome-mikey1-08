67b9216bebd2f7747125cedf7d1aa2627f12876f
Michael Li <liakr@bc.edu>
2022-01-31 11:09:20 -0500

    displays image0!

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍🏼 viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
     }
 
     @IBAction func messageButtonPressed(_ sender: Any) {
-        print("😎The message button was pressed!")
         messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "image0")
     }
     
 }
