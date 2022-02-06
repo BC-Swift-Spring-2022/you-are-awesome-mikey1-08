ad0c5dd72917aed24370e3fe151a20ff437e57b7
Michael Li <liakr@bc.edu>
2022-02-06 15:02:53 -0500

    Ch. 2.13 Getting Loopy with While and Repeat

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,27 +31,21 @@ class ViewController: UIViewController {
                         "You Are Fantastic!",
                         "Fabulous? That's You!", "You've Got The Design Skills of Jony Ive"]
         
-        messageLabel.text =  messages[Int.random(in:0...messages.count-1)]
-        imageView.image = UIImage(named: "image\(Int.random(in:0...9))")
-        
-//        messageLabel.text = messages[messageNumber]
-//        messageNumber += 1
-//        if messageNumber == messages.count {messageNumber=0}
-//        let imageName = "image" + String(imageNumber)
-//        let imageName = "image\(imageNumber)"
-//        imageView.image = UIImage(named: imageName)
-//        imageNumber = imageNumber + 1
-//        if imageNumber == 10 {imageNumber = 0}
-//
-//        if messageLabel.text == awesomeMessage {
-//            messageLabel.text = greatMessage
-//            imageView.image = UIImage(named: "image1")
-//        } else if messageLabel.text == greatMessage {
-//            messageLabel.text = bombMessage
-//            imageView.image = UIImage(named: "image2")
-//        } else {
-//            messageLabel.text = awesomeMessage
-//            imageView.image = UIImage(named: "image0")
-//        }
+        var newMessageNumber: Int
+        repeat {
+            print("Repeating Value")
+            newMessageNumber = Int.random(in:0...messages.count-1)
+//        messageLabel.text =  messages[Int.random(in:0...messages.count-1)]
+        } while messageNumber == newMessageNumber
+        messageNumber = newMessageNumber
+        messageLabel.text = messages[messageNumber]
+    
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in:0...9)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
     }
+    
 }
