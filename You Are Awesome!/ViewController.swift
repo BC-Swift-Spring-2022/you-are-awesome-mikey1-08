0cbbbb93a62a6a25008baacca11bbb77781d450e
Michael Li <liakr@bc.edu>
2022-02-06 18:23:20 -0500

    Ch. 2.15 Play Sounds

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
     
     var imageNumber = -1
     var messageNumber = -1
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +48,17 @@ class ViewController: UIViewController {
         } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }   catch {
+                print("ERROR: Could not initialize AVAudioPlayer")
+            }
+        } else {
+            print("ERROR: Could not read data from file sound 0")
+        }
     }
     
 }
